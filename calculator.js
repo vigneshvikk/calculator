3ac9af7fe2d9173fa8266082e78884889b80faee
unknown <vigneshvikky199923@gmail.com>
2023-01-26 14:06:22 +0530

    first cimmit

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function bclick(val){
+    document.getElementById("screen").value+=val;
+
+}
+function cleardisplay()
+{
+    document.getElementById("screen").value=""
+}
+function equalclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+
+
+
+
+}
